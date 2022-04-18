8a571e7eb27fc32ce2c111a55e7e1a24b07d3473
javimiglie <javimiglie@gmail.com>
2022-04-18 12:14:47 -0300

    first commit

---
+++ Js/script.js
@@ -0,0 +1,144 @@
+let carritoDeCompras = []
+
+const contenedorProductos = document.getElementById('contenedor-productos');
+const contenedorCarrito = document.getElementById('carrito-contenedor');
+
+//const botonTerminar = document.getElementById('terminar')
+//const finCompra = document.getElementById('fin-compra')
+
+const contadorCarrito = document.getElementById('contadorCarrito');
+const precioTotal = document.getElementById('precioTotal');
+
+const selecPrecio = document.getElementById('selecPrecio')
+const buscador = document.getElementById('search')
+
+
+
+//Falta Crear Filtro para ordenar por $
+//Falta finalizar compra en carrito
+
+
+
+
+
+
+
+function mostrarProductos(array) {
+    contenedorProductos.innerHTML = ""
+
+    array.forEach(item => {
+
+        let div = document.createElement("div")
+        div.classList.add("producto")
+
+        div.innerHTML += `
+                    <div class="card">
+                        <div class="card-image">
+                            <img src=${item.img}>
+                            <span class="card-title">${item.nombre}</span>
+
+                        </div>
+                        <div class="card-content">
+                            <p>${item.descripcion}</p>
+                            <p> $${item.precio}</p>
+                            <a  id="agregar${item.id}" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add_shopping_cart</i></a>
+                        </div>
+                    </div>
+    `
+        contenedorProductos.appendChild(div)
+
+        let btnAgregar = document.getElementById(`agregar${item.id}`)
+
+        btnAgregar.addEventListener("click", () => {
+            agregarAlCarrito(item.id)
+        })
+
+    })
+};
+
+
+function agregarAlCarrito(id) {
+    let yaEsta = carritoDeCompras.find(item => item.id == id)
+    if (yaEsta) {
+        yaEsta.cantidad = yaEsta.cantidad + 1
+        document.getElementById(`und${yaEsta.id}`).innerHTML = ` <p id=und${yaEsta.id}>Und:${yaEsta.cantidad}</p>`
+        actualizarCarrito()
+    } else {
+        let productoAgregar = stockProductos.find(elemento => elemento.id == id)
+
+        productoAgregar.cantidad = 1
+
+        carritoDeCompras.push(productoAgregar)
+
+        actualizarCarrito()
+
+        mostrarCarrito(productoAgregar)
+    }
+
+    localStorage.setItem("carrito", JSON.stringify(carritoDeCompras))
+
+};
+
+
+
+
+
+function mostrarCarrito(productoAgregar) {
+
+    let div = document.createElement("div")
+    div.className = "productoEnCarrito"
+    div.innerHTML = `
+                    <p>${productoAgregar.nombre}</p>
+                    <p>Precio: $${productoAgregar.precio}</p>
+                    <p id="und${productoAgregar.id}">Und:${productoAgregar.cantidad}</p>
+                    <button id="eliminar${productoAgregar.id}" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+    `
+    contenedorCarrito.appendChild(div)
+
+    let btnEliminar = document.getElementById(`eliminar${productoAgregar.id}`)
+
+    btnEliminar.addEventListener("click", () => {
+        if (productoAgregar.cantidad == 1) {
+            btnEliminar.parentElement.remove()
+            carritoDeCompras = carritoDeCompras.filter(item => item.id != productoAgregar.id)
+            actualizarCarrito()
+            localStorage.setItem("carrito", JSON.stringify(carritoDeCompras))
+        } else {
+            productoAgregar.cantidad = productoAgregar.cantidad - 1
+            document.getElementById(`und${productoAgregar.id}`).innerHTML = ` <p id=und${productoAgregar.id}>Und:${productoAgregar.cantidad}</p>`
+            actualizarCarrito()
+            localStorage.setItem("carrito", JSON.stringify(carritoDeCompras))
+        }
+
+
+    })
+
+
+};
+
+
+
+function actualizarCarrito() {
+    contadorCarrito.innerText = carritoDeCompras.reduce((acc, el) => acc + el.cantidad, 0)
+    precioTotal.innerText = carritoDeCompras.reduce((acc, el) => acc + (el.precio * el.cantidad), 0)
+};
+
+
+
+
+function recuperar() {
+    let recuperarLS = JSON.parse(localStorage.getItem('carrito'))
+
+    if (recuperarLS) {
+        recuperarLS.forEach(el => {
+            mostrarCarrito(el)
+            carritoDeCompras.push(el)
+            actualizarCarrito()
+        })
+    }
+
+
+};
+
+mostrarProductos(stockProductos);
+recuperar();
---
+++ Js/stock.js
@@ -0,0 +1,15 @@
+let stockProductos = [
+    { id: 1, nombre: "Clasic Acero", descripcion: "Electrodoméstico", precio: 52530, img: "./Media/clasic-ACERO.jpg" },
+    { id: 2, nombre: "Clasic Acero Espejada", descripcion: "Electrodoméstico", precio: 67611, img: "./Media/clasic-ACERO.jpg" },
+    { id: 3, nombre: "Clasic Light", descripcion: "Electrodoméstico", precio: 56712, img: "./Media/clasic-ACERO.jpg" },
+    { id: 4, nombre: "Eco Acero", descripcion: "Electrodoméstico", precio: 53438, img: "./Media/clasic-ACERO.jpg" },
+    { id: 5, nombre: "Eco Acero Espejada", descripcion: "Electrodoméstico", precio: 60839, img: "./Media/clasic-ACERO.jpg" },
+    { id: 6, nombre: "Eco Light", descripcion: "Electrodoméstico", precio: 51725, img: "./Media/clasic-ACERO.jpg" },
+    { id: 7, nombre: "Euro Acero Espejada", descripcion: "Electrodoméstico", precio: 66567, img: "./Media/clasic-ACERO.jpg" },
+    { id: 8, nombre: "Euro Acero Espejada AG", descripcion: "Electrodoméstico", precio: 74613, img: "./Media/clasic-ACERO.jpg" },
+    { id: 9, nombre: "Euro Acero Grafito", descripcion: "Electrodoméstico", precio: 60948, img: "./Media/clasic-ACERO.jpg" },
+    { id: 10, nombre: "Euro Acero Grafito AG", descripcion: "Electrodoméstico", precio: 74613, img: "./Media/clasic-ACERO.jpg" },
+    { id: 11, nombre: "Grand Chef", descripcion: "Electrodoméstico", precio: 59539, img: "./Media/clasic-ACERO.jpg" },
+    { id: 12, nombre: "Grand Chef Espejada AG", descripcion: "Electrodoméstico", precio: 75328, img: "./Media/clasic-ACERO.jpg" },
+    { id: 13, nombre: "Grand Chef Espejada", descripcion: "Electrodoméstico", precio: 64180, img: "./Media/clasic-ACERO.jpg" }
+];
